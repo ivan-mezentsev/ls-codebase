50bba6e23c58c42ce2332a48e0a5a7b5b4b6aafd
LamkaSlim <LamkaSlim@localhost>
2011-03-29 12:43:13 +0400

    Added script for sipnet balance check

---
+++ sipnet/sipnet_checkbalance.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import urllib
+import urllib2
+
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
+
+try:
+	params = urllib.urlencode({'Password' : sys.argv[2], 'Name' : sys.argv[1], 'CabinetAction' : 'login'} )
+except:
+	print "No valid arguments supplied. Please run me as 'sipnet_checkbalance.py login password'"
+	sys.exit()
+
+req = urllib2.Request("https://customer.sipnet.ru/cabinet/",params,headers)
+
+try:
+	response = urllib2.urlopen(req)
+	pureHtml = response.read()
+except:
+	print "Can't connect to https://customer.sipnet.ru/cabinet/"
+	sys.exit()
+
+
+endIndex = pureHtml.find('&nbsp;<span style="font-size: 80%">')
+startIndex = pureHtml.rfind('<div>',0, endIndex)
+balance = pureHtml[startIndex+5:endIndex]
+try:
+	float(balance)
+	print balance;
+except:
+	print "No valid balance string found. Sipnet html changed or no login/password is incorrect"
+	sys.exit()
